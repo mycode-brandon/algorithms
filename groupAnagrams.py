1c5e11c5b295487994b7089fec784792c0feaf5b
NTQE <ntqextra@outlook.com>
2023-08-22 10:06:46 -0500

    Group anagrams not currently working on 3/119 test cases.

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,15 +1,22 @@
 # Same as isAnagram, but we need to additionally group the anagrams together in sub lists with list of words as input
 # Let's try using our ascii character mapping method
 
+# currently not working because if there are greater than 9 elements the digit rolls from 9 to 10, which doesn't convert well to string value
 def groupAnagrams(strs: list[str]) -> list[list[str]]:
     result = {}
     for word in strs:
         character_map = [0] * 26
         for character in word:
             character_map[ord(character) - ord('a')] += 1
-        result[''.join(character_map)].append(word)
-    return result.values()
+        key = ''.join([str(i) for i in character_map])
+        print("key: ", key, "word: ", word, "length of key: ", len(key))
+        if key in result:
+            result[key].append(word)
+        else:
+            result[key] = [word]
+    return list(result.values())
 
 
-groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(groupAnagrams(["bdddddddddd","bbbbbbbbbbc"]))
 
