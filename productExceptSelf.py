cfe25165a2f547969d1c6d0815d75fd99a19dc2e
NTQE <ntqextra@outlook.com>
2023-08-22 15:26:25 -0500

    Update repo.

---
+++ productExceptSelf.py
@@ -0,0 +1,6 @@
+# Without being able to divide, makes this interesting. I felt like there needed to be some utility arrays to
+# get this complete, but couldn't quite figure our the trick. After some research, found the prefix/postfix method and
+# that can even be implemented without creating any arrays other than the result.
+
+def productExceptSelf(nums: list[int]) -> list[int]:
+
